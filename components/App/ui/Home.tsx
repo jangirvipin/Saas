27fb103db5e4eb8dd5cb8ad91b37ee2bdd29b14e
jangirvipin <2341020@sliet.ac.in>
2025-03-07 00:59:34 +0530

    SOme

--- components/App/ui/Home.tsx
+++ components/App/ui/Home.tsx
@@ -43,9 +43,7 @@ export default function HeroSection() {
                 </div>
             </div>
 
-            <div className="absolute inset-0 pointer-events-none opacity-20">
-                <div className="absolute inset-0 bg-[size:20px_20px] bg-[linear-gradient(to_right,#ffffff12_1px,transparent_1px),linear-gradient(to_bottom,#ffffff12_1px,transparent_1px)]"></div>
-            </div>
+
         </div>
     );
 }
