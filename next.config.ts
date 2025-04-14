c6ebc5e86f9fa140ab7c5abdce33e79bba1c5c09
Prahalya <Prahek@gmail.com>
2025-04-14 18:02:31 +0530

    Added Github nav icon

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-    webpackDevMiddleware: (config: any) => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 500
-        };
-        return config;
-    },
-};
+   // webpackDevMiddleware: (config: any) => {
+    //    config.watchOptions = {
+      //      poll: 1000,
+       //     aggregateTimeout: 500
+      //  };
+     //   return config;
+     //}
+     reactStrictMode: true,
+
+    };
 
 export default nextConfig;
